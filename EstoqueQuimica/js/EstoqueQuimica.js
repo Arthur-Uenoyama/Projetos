28e8198c1f3ca3620261d24f65e146a884193d48
Arthur Uenoyama Dezem <uenoarthur@outlook.com>
2024-10-10 09:14:58 -0300

    Delete EstoqueQuimica directory

--- EstoqueQuimica/js/EstoqueQuimica.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('wheel', function(event) {
-        if (event.deltaY > 0) {
-            console.log("Rolou para baixo");
-        } else {
-            console.log("Rolou para cima");
-        }
-    });
